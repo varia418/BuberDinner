6d72ab375299ec22d40cf4c77aa2d4a6c4aced94
varia <ltnhan2610@gmail.com>
2024-04-02 15:47:19 +0700

    remove Application/Services

--- src/BuberDinner.Application/Services/Authentication/Commands/IAuthenticationCommandService.cs
+++
@@ -1,8 +0,0 @@
-using ErrorOr;
-
-namespace BuberDinner.Application.Services.Authentication.Commands;
-
-public interface IAuthenticationCommandService
-{
-    ErrorOr<AuthenticationResult> Register(string firstName, string lastName, string email, string password);
-}
--- src/BuberDinner.Application/Services/Authentication/Queries/IAuthenticationQueryService.cs
+++
@@ -1,8 +0,0 @@
-using ErrorOr;
-
-namespace BuberDinner.Application.Services.Authentication.Queries;
-
-public interface IAuthenticationQueryService
-{
-    ErrorOr<AuthenticationResult> Login(string email, string password);
-}
